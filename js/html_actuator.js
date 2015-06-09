5dc93baee3a47006a4d22afe162e7cceacafb585
Jum <jum@playmoolah.com>
2015-06-09 16:12:23 +0800

    Changed Tile 2 to an image

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -62,7 +62,14 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+  //inner.textContent = tile.value;
+  switch (tile.value)
+  {
+  	case 2:
+  		break;
+  	default:
+  		inner.textContent = tile.value;
+  }
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
